de2b57e17c5da652d47f7a82467afc6c0a6ff677
liujunshuai <2197004003@qq.com>
2023-01-20 20:37:12 +0800

    111

---
+++ src/main/java/com/ljs/git/gitTest.java
@@ -0,0 +1,11 @@
+package com.ljs.git;
+
+/**
+ * @author shkstart
+ * @create 2023-01-20 20:35
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
